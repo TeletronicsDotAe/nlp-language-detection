3fb657515c2588ad8e34d3fc4736cdce9b34c2c9
Boris Søborg Jensen <boris.jensen@d4tec.com>
2016-02-15 12:47:36 +0100

    Sensible detectAll implementation in OptimaizeFallback

--- src/main/java/ae/teletronics/nlp/OptimaizeFallback.java
+++ src/main/java/ae/teletronics/nlp/OptimaizeFallback.java
@@ -4,6 +4,8 @@ import com.google.common.base.Optional;
 import com.neovisionaries.i18n.LanguageCode;
 import com.optimaize.langdetect.i18n.LdLocale;
 
+import java.util.List;
+
 /**
  * Created by Boris on 12-02-2016.
  */
@@ -15,5 +17,14 @@ public class OptimaizeFallback extends Optimaize implements LanguageDetector {
     public LanguageCode detect(String text) {
         Optional<LdLocale> lang = instance.detect(text);
         return lang.isPresent() ? LanguageMapper.fromString(lang.get().getLanguage()) : fallback.detect(text);
+    }
+
+    @Override
+    public List<LanguageProbability> detectAll(String text) {
+        List<LanguageProbability> ps = super.detectAll(text);
+        if (ps.size() > 0)
+            return ps;
+        else
+            return fallback.detectAll(text);
     }
 }
