fab3dd3d7513ebe5872ce1a284d3229dc02d6be3
Boris Søborg Jensen <boris.jensen@d4tec.com>
2016-02-15 15:04:55 +0100

    Default detectMostProbable implementation filters on threshold probability and returns highest probability.

--- src/main/java/ae/teletronics/nlp/LanguageDetector.java
+++ src/main/java/ae/teletronics/nlp/LanguageDetector.java
@@ -12,9 +12,9 @@ import java.util.Optional;
 public interface LanguageDetector {
     LanguageCode detect(String text);
 
-    default Optional<LanguageProbability> detectMostProbable(String text) {
+    default Optional<LanguageProbability> detectMostProbable(String text, double threshold) {
         try {
-            return detectAll(text).stream().sorted(Comparator.comparing(lp -> lp.getProbability())).findFirst();
+            return detectAll(text).stream().filter(lp -> lp.getProbability() > threshold).sorted(Comparator.comparingDouble(lp -> - lp.getProbability())).findFirst();
         } catch (Exception e) {
             System.err.println(e);
             return Optional.empty();
