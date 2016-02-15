ac8761f6df290cea69e2d9f2c3e4eaf10e0f1ae6
Boris Søborg Jensen <boris.jensen@d4tec.com>
2016-02-15 12:49:13 +0100

    LanguageDetector interface has detectMostProbable with default implementation.

--- src/main/java/ae/teletronics/nlp/LanguageDetector.java
+++ src/main/java/ae/teletronics/nlp/LanguageDetector.java
@@ -2,13 +2,24 @@ package ae.teletronics.nlp;
 
 import com.neovisionaries.i18n.LanguageCode;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by hhravn on 10/02/16.
  */
 public interface LanguageDetector {
     LanguageCode detect(String text);
+
+    default Optional<LanguageProbability> detectMostProbable(String text) {
+        try {
+            return detectAll(text).stream().sorted(Comparator.comparing(lp -> lp.getProbability())).findFirst();
+        } catch (Exception e) {
+            System.err.println(e);
+            return Optional.empty();
+        }
+    }
 
     List<LanguageProbability> detectAll(String text);
 }
