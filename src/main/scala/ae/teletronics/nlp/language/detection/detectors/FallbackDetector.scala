6a242105eeaab7be9d0e94ab983abe946ae28e1f
Boris Søborg Jensen <boris.jensen@d4tec.com>
2016-06-29 15:58:11 +0200

    FallbackDetectorTest#testHindi test succeds

--- src/main/scala/ae/teletronics/nlp/language/detection/detectors/FallbackDetector.scala
+++ src/main/scala/ae/teletronics/nlp/language/detection/detectors/FallbackDetector.scala
@@ -8,7 +8,7 @@ class FallbackDetector(detectors: List[SLanguageDetector]) extends SLanguageDete
   override def detect(text: String): List[Language] = {
     detectors
       .iterator // iterators are lazy, so the following map and find only evaluates as much as needed
-      .map(_.detect(text))
+      .map(detector => detector.detect(text).filter(_.confidence >= detector.minimalConfidence()))
       .find(!_.isEmpty)
       .getOrElse(List.empty[Language])
   }
